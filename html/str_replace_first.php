64c9184b0fb0e00fc2d437392df57313cfe7e987
root <root@net-control.us>
2022-06-23 20:01:26 +0000

    remove extra backup copy

--- html/str_replace_first.php
+++
@@ -1,9 +0,0 @@
-<?php
-function str_replace_first($search, $replace, $subject) {
-	$pos = strpos($subject, $search);
-		if ($pos !== false) {
-			return substr_replace($subject, $replace, $pos, strlen($search));
-		}
-		return $subject;
-	}
-?>
