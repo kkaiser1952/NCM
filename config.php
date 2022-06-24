a937ded0ab12b5605b4072ae5e54217e016c80a8
root <root@net-control.us>
2022-06-24 20:29:30 +0000

    setting up gitignore to ignore config and the phpmyadmin files

--- config.php
+++ config.php
@@ -2,15 +2,11 @@
 
 /* Put ALL API Keys and DB Passwords, etc in this file. */
 
-
 // https://openweathermap.org/
-$_API_OPEN_WXMAP_APPID='4a2328021bf09254755b906723b350e2';
+$_API_OPEN_WXMAP_APPID='';
 
 // http://extreme-ip-lookup.com/
-$_API_EXTREME_IP_KEY='T2XpPubMpPZ7r58WHeWw';
-
-
-
+$_API_EXTREME_IP_KEY='';
 
 
 ?>
