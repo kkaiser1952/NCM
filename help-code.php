a87cc884c3abbaf4e7f6b4186bf7a665570113f4
root <root@net-control.us>
2022-10-14 17:00:01 +0000

    Sync from production 2022-10-14 17:00:01.095199931+00:00

--- help-code.php
+++ help-code.php
@@ -95,6 +95,144 @@
         <a href="#buildclosinglisting">buildClosingListing.php</a><br>
         <a href="#builddwtt">buildDWtt.php</a><br>
         <a href="#buildeventdb">buildEventListing.php</a><br>
+        <a href="#buildevents">buildEvents.php</a><br>
+        <a href="#buildfacilitydropdown">buildFacilityDropDown.php</a><br>
+        
+        <a href="#buildfsqheardlist">buildFSQHeardList.php</a><br>
+        <a href="#buildgrouplist">buildGroupList.php</a><br>
+        <a href="#buildheardlist">buildHeardList.php</a><br>
+        <a href="#buildnewgroup">BuildNewGroup.php</a><br>
+        <a href="#buildoptionsforselect">buildOptionsForSelect.php</a><br>
+        <a href="#buildourfrequencies">buildourFrequencies.php</a><br>
+        <a href="#buildpoi">buildPoi.php</a><br>
+        
+        <a href="#buildpreamblelisting">buildPreambleListing.php</a><br>
+        <a href="#buildrightcorner">buildRightCorner.php</a><br>
+        <a href="#buildstationstable">buildStationsTable.php</a><br>
+        <a href="#buildsubnetcandidates">buildSubNetCandidates.php</a><br>      
+        <a href="#buildthreedropdowns">buildThreeDropdowns.php</a><br>
+        <a href="#builduniquecalllist">buildUniqueCallList.php</a><br>
+        <a href="#buildupperrightcorner">buildUpperRightCorner.php</a><br>
+        
+        <a href="#buttonbar">buttonBar.php</a><br>
+        <a href="#checkfornewcall">checkForNewCall.php</a><br>
+        <a href="#checkin">checkin.php</a><br>
+        <a href="#checksection">checkSection.php</a><br>
+        <a href="#classlist">classList.php</a><br>
+        <a href="#clear_all_tactical">Clear_All_Tactical.php</a><br>
+        <a href="#clonepb">closePB.php</a><br>
+        
+        <a href="#closelog">closeLog.php</a><br>
+        <a href="#colcolorassign">colColorAssign.php</a><br>
+        <a href="#column_info">column_info.html</a><br>
+        <a href="#columnpicker">columnPicker.php</a><br>
+        <a href="#config">config.php</a><br>
+        <a href="#converttopb">convertToPB.php</a><br>
+        
+        <a href="#cool-ont-graphics">cool-Font-Graphics.html</a><br>
+        <a href="#csvfromnetlog">CSVfromNetLog.php</a><br>
+        <a href="#dbconnectdtls">dbConnectDtls.php</a><br>
+        <a href="#delete-row">delete-row.php</a><br>
+        <a href="#deleteevent">deleteEvent.php</a><br>
+        <a href="#dropdowns">dropdowns.php</a><br>
+        <a href="#editfccandstations">editFCCandStations.php</a><br>
+        
+        <a href="#editstationstable">editStationsTable.php</a><br>
+        <a href="#env_setup">ENV_SETUP.php</a><br>
+        <a href="#fcc_lookup">fcc_lookup.php</a><br>
+        <a href="#fielddayreport">fielddayReport.php</a><br>
+        <a href="#findreports">findReports.php</a><br>
+        <a href="#fixhome">fixHome.php</a><br>
+        <a href="#fixlatlongrid">fixLatLonGrid.php</a><br>
+        
+        <a href="#fixstationwithfcc">fixStationWithFCC.php</a><br>
+        <a href="#geocode">geocode.php</a><br>
+        <a href="#Geocoder">Geocoder.php</a><br>
+        <a href="#getactivities">getactivities.php</a><br>
+        <a href="#getcallhistory">getCallHistory.php</a><br>
+        <a href="#getcallorderbynet">getCallOrderByNet.php</a><br>
+        <a href="#getcenterfromdegrees">getCenterFromDegrees.php</a><br>
+        
+        <a href="#getcookie">getCookie.php</a><br>
+        <a href="#getcrossroads">getCrossRoads.php</a><br>
+        <a href="#getdata4dashboard">getData4Dashboard.php</a><br>
+        <a href="#getdistancewlatlon">getDistanceWlatlon.php</a><br>
+        <a href="#getDXstationInfo">getDXstationInfo.php</a><br>
+        <a href="#getfccrecord">getFCCrecord.php</a><br>
+        <a href="#getgencomments">getGenComments.php</a><br>
+        
+        <a href="#getgridcounts">getGridCounts.php</a><br>
+        <a href="#getgridlatlnglname">getGridLatLngLname.php</a><br>
+        <a href="#gethint">gething.php</a><br>
+        <a href="#gethintname">gethintName.php</a><br>
+        <a href="#gethingsuspects">gethintSuspects.php</a><br>
+        <a href="#getjsonrecord">getJSONrecord.php</a><br>
+        <a href="#getkind">getkind.php</a><br>
+        
+        <a href="#getlatlonfromgrid">getLatLonFromGrid.php</a><br>
+        <a href="#getlistoffacilities">getListOfFacilities.php</a><br>
+        <a href="#getncmxml">getNCMXML.php</a><br>
+        <a href="#getnetemails">getNetEmails.php</a><br>
+        <a href="#getnewwx">getNewWX.php</a><br>
+        <a href="#getopennets">getOpenNets.php</a><br>
+        <a href="#getqrz">getQRZ.php</a><br>
+        
+        <a href="#getrealipaddr">getRealIpAddr.php</a><br>
+        <a href="#getstationics-214a">getStationICS-214A.php</a><br>
+        <a href="#getstationtimeline">getStationTimeLine.php</a><br>
+        <a href="#getsubnet">getsubnets.php</a><br>
+        <a href="#gettimelog">getTimeLog.php</a><br>
+        <a href="#getwx">getWX.php</a><br>
+        <a href="#googlecharts">googleCharts.php</a><br>
+        
+        <a href="#graph_attendance">graph_attendance.php</a><br>
+        <a href="#graph_by_years">graph_by_years.php</a><br>
+        <a href="#graphanet_ploty">graphAnet_Ploty.php</a><br>
+        <a href="#gridmap">gridmap.php</a><br>
+        <a href="#gridsquare">GridSquare.php</a><br>
+        <a href="#groupscorecard">groupScoreCard.php</a><br>
+        <a href="#hamnets">hamnets.php</a><br>
+        
+        <a href="#headerdefinitions">headerDefinitions.php</a><br>
+        <a href="#heart">heart.php</a><br>
+        <a href="#help">help.php</a><br>
+        <a href="#helphtml">help.html</a><br>
+        <a href="#help-code">help-code.php</a><br>
+        <a href="#hztimeline">HzTimeline.php</a><br>
+        <a href="#ics205a">ics205A.php</a><br>
+        
+        <a href="#ics214">ics214.php</a><br>
+        <a href="#ics309">ics309.php</a><br>
+        <a href="#index">index.php</a><br>
+        <a href="#inserttostations">insertToStations.php</a><br>
+        <a href="#joinedbullets">joinedbullets.php</a><br>
+        <a href="#latlongdistance">latlongdistance.php</a><br>
+        <a href="#listallpois">listAllPOIs.php</a><br>
+        
+        <a href="#listnets">listNets.php</a><br>
+        <a href="#map">map.php</a><br>
+        <a href="#map4743pagesource">map4743pagesource.php</a><br>
+        <a href="#metaedits">metaEdits.php</a><br>
+        <a href="#multilevel">multilevel.html</a><br>
+        <a href="#ncmapi">NCMapi.php</a><br>
+        <a href="#ncmmaps">NCMMaps.php</a><br>
+        
+        <a href="#builddwtt">buildDWtt.php</a><br>
+        <a href="#builddwtt">buildDWtt.php</a><br>
+        <a href="#builddwtt">buildDWtt.php</a><br>
+        <a href="#builddwtt">buildDWtt.php</a><br>
+        <a href="#builddwtt">buildDWtt.php</a><br>
+        <a href="#builddwtt">buildDWtt.php</a><br>
+        <a href="#builddwtt">buildDWtt.php</a><br>
+        
+        <a href="#builddwtt">buildDWtt.php</a><br>
+        <a href="#builddwtt">buildDWtt.php</a><br>
+        <a href="#builddwtt">buildDWtt.php</a><br>
+        <a href="#builddwtt">buildDWtt.php</a><br>
+        <a href="#builddwtt">buildDWtt.php</a><br>
+        <a href="#builddwtt">buildDWtt.php</a><br>
+        <a href="#builddwtt">buildDWtt.php</a><br>
+
 	 </nav>
 <div>
     <a class="gotoindex" href="#index">Back to the Index</a>
