4c9bab608dcee32e9e69006bb8de44e075e5ba37
root <root@net-control.us>
2023-02-27 02:00:01 +0000

    Sync from production 2023-02-27 02:00:01.494139799+00:00

--- help.php
+++ help.php
@@ -26,7 +26,7 @@
 <div id="banner">
 
 	<div class="title">
-		<p>Netz	</p>
+		<p>Net	</p>
 		<p>Control  <span style="float: right;">Documentation &amp; Help</span></p>
 		<p>Manager</p>
 	</div> <!-- End title -->
