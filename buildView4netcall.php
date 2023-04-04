11f3c025fa415db3f52b8b97748599bef553311d
root <root@net-control.us>
2023-04-04 17:00:01 +0000

    Sync from production 2023-04-04 17:00:01.784451699+00:00

--- buildView4netcall.php
+++ buildView4netcall.php
@@ -13,34 +13,48 @@ If no callsign is found in the FCC or foreign DB it will be added to the NET wit
 */
 
 require_once "dbConnectDtls.php";
-
-	//$q = $_GET['q']; // netcall of net being built
 	
-	$q = 'CREW2273'; 
+	$dbname = 'ncm';
+	$viewnm = 'CREW2273'; 
+	
+	// Check if the view already exists
+    $sql_check = "SELECT COUNT(*) as count FROM information_schema.views WHERE table_schema = '$dbname' AND table_name = '$viewnm'";
+    $stmt_check = $conn->prepare($sql_check);
+    $stmt_check->execute();
+    $result_check = $stmt_check->fetch(PDO::FETCH_ASSOC);
 
-$sql = (
-    "CREATE OR REPLACE VIEW ncm.$q AS
-        SELECT DISTINCT st.callsign, st.Fname, 
-    	    st.Lname, st.email, st.tactical, 
-    	    st.latitude, st.longitude, st.grid, 
-                st.county, 	 st.state, 	   st.city, 
-                st.district, st.creds, 	   st.country, 
-                CONCAT(st.Fname, ' ', st.Lname, ' --> ', st.state, '--', st.county, '--', st.district) AS name
-          FROM ncm.stations st
-          JOIN ncm.NetLog nl ON st.callsign = nl.callsign
-         WHERE nl.netcall = '$q'
-           AND nl.callsign NOT LIKE '%NONHAM%'
-           AND nl.callsign NOT LIKE '%EMCOMM%'
-           AND nl.logdate >= DATE_SUB(CURRENT_DATE(), INTERVAL 6 MONTH)
-           AND nl.logdate < CURRENT_DATE()
-           
-    ");
-        echo "<br>$sql<br>";
-        
-        $stmt = $db_found->prepare($sql);
-        execute($stmt); 
-        
-        
-        //$sql->execute();
-        
+    // Create the SQL statement to create the view if it doesn't exist
+    if ($result_check['count'] == 0) {
+        $sql_create = 
+            "CREATE VIEW $viewnm AS
+                SELECT DISTINCT st.callsign, st.Fname, 
+            	    st.Lname, st.email, st.tactical, 
+            	    st.latitude, st.longitude, st.grid, 
+                        st.county, 	 st.state, 	   st.city, 
+                        st.district, st.creds, 	   st.country, 
+                        CONCAT(st.Fname, ' ', st.Lname, ' --> ', st.state, '--', st.county, '--', st.district) AS name
+                  FROM ncm.stations st
+                  JOIN ncm.NetLog nl ON st.callsign = nl.callsign
+                 WHERE nl.netcall = '$viewnm'
+                   AND nl.callsign NOT LIKE '%NONHAM%'
+                   AND nl.callsign NOT LIKE '%EMCOMM%'
+                   AND nl.logdate >= DATE_SUB(CURRENT_DATE(), INTERVAL 6 MONTH)
+                   AND nl.logdate < CURRENT_DATE()  
+            ";
+            
+            try {
+                // Prepare the statement
+                $stmt_create = $conn->prepare($sql_create);
+            
+                // Execute the statement
+                $stmt_create->execute();
+            
+                // Deallocate the statement
+                $stmt_create = null;
+            
+              } catch(PDOException $e) {
+                echo "Error: " . $e->getMessage();
+              }
+    } // End if($result_check...      
+
 ?>
