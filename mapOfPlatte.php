5a4de719adfd7bdd4cea0251b88a60b0d6d2940e
root <root@net-control.us>
2023-04-10 17:00:02 +0000

    Sync from production 2023-04-10 17:00:02.040205680+00:00

--- mapOfPlatte.php
+++ mapOfPlatte.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
+<!-- Just a test map of Platte and Clay counties -->
+<!-- Suggested by ChatGPT -->
+<!-- map.setView([39.3172, -94.5123], 10); -->
 
-<!-- Just a test map o platte county -->
-<!-- Suggested by chatGPT -->
+
 
 <html>
 <head>
@@ -13,14 +15,9 @@
   <!-- Include Leaflet JavaScript -->
   <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
   
-  <script src="js/Leaflet.Graticule.js"></script>
-
-
-  
   <style>
     #map {
-     
-      height: 1000px;
+      height: 800px;
     }
   </style>
 </head>
@@ -28,51 +25,71 @@
   <div id="map"></div>
   
   <script>
-// Create a new map instance
-var map = L.map('map');
+    // Create a new map instance
+    var map = L.map('map');
 
-// Set the map view to Platte County
-map.setView([39.3621, -94.7722], 12);
+    // Set the map view to Platte County
+    map.setView([39.3621, -94.7722], 10);
 
-// Add the OpenStreetMap tile layer to the map
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-  maxZoom: 18
-}).addTo(map);
+    // Add the OpenStreetMap tile layer to the map
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+      maxZoom: 18
+    }).addTo(map);
 
-// Load the GeoJSON data for Platte County from OpenStreetMap
-fetch('https://nominatim.openstreetmap.org/search.php?q=Platte+County+Missouri&polygon_geojson=1&format=geojson')
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
-    // Create a new GeoJSON layer with the county boundary data
-    var countyLayer = L.geoJSON(data);
+    // Load the GeoJSON data for Platte County from OpenStreetMap
+    fetch('https://nominatim.openstreetmap.org/search.php?q=Platte+County+Missouri&polygon_geojson=1&format=geojson')
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        // Create a new GeoJSON layer with the county boundary data
+        var countyLayer = L.geoJSON(data);
 
-    // Add the county layer to the map
-    countyLayer.addTo(map);
+        // Add the county layer to the map
+        countyLayer.addTo(map);
 
-    // Calculate the bounds of the county layer
-    var countyBounds = countyLayer.getBounds();
+        // Calculate the bounds of the county layer
+        var countyBounds = countyLayer.getBounds();
+
+        // Fit the map to the bounds of the county layer
+        map.fitBounds(countyBounds);
+      });
+      
+    // Load the GeoJSON data for Clay County from OpenStreetMap
+    fetch('https://nominatim.openstreetmap.org/search.php?q=Clay+County+Missouri&polygon_geojson=1&format=geojson')
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        // Create a new GeoJSON layer with the county boundary data
+        var countyLayer = L.geoJSON(data);
+
+        // Add the county layer to the map
+        countyLayer.addTo(map);
+
+        // Calculate the bounds of the county layer
+        var countyBounds = countyLayer.getBounds();
+
+        // Fit the map to the bounds of the county layer
+        map.fitBounds(countyBounds);
+      });
 
     // Create a new Graticule instance with the desired options
-    var graticule = L.graticule({
-        interval: 2,
-        style: {
-            color: '#333',
-            weight: 1,
-            opacity: 0.5
-        }
-    }).addTo(map);
-    
+    var graticule = L.latLngGraticule({
+      interval: 1,
+      showLabel: true,
+      opacity: 0.5,
+      fontColor: '#333'
+    });
+
+    // Add the Graticule layer to the map
+    graticule.addTo(map);
+
+    // Bring the Graticule layer to the front of the map
     graticule.bringToFront();
-
-
-    // Fit the map to the bounds of the county layer
-    map.fitBounds(countyBounds);
-  });
-
 
   </script>
 </body>
 </html>
+
