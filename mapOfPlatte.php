c8e3a01be801d9b9a69d434e4f2c8c45751be93e
root <root@net-control.us>
2023-04-10 16:00:01 +0000

    Sync from production 2023-04-10 16:00:01.152454453+00:00

--- mapOfPlatte.php
+++ mapOfPlatte.php
@@ -13,6 +13,10 @@
   <!-- Include Leaflet JavaScript -->
   <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
   
+  <script src="js/Leaflet.Graticule.js"></script>
+
+
+  
   <style>
     #map {
      
@@ -24,11 +28,11 @@
   <div id="map"></div>
   
   <script>
-      // Create a new map instance
+// Create a new map instance
 var map = L.map('map');
 
 // Set the map view to Platte County
-map.setView([39.3621, -94.7722], 10);
+map.setView([39.3621, -94.7722], 12);
 
 // Add the OpenStreetMap tile layer to the map
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -36,8 +40,8 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 18
 }).addTo(map);
 
-// Load the GeoJSON data for Platte County
-fetch('platte-county.geojson')
+// Load the GeoJSON data for Platte County from OpenStreetMap
+fetch('https://nominatim.openstreetmap.org/search.php?q=Platte+County+Missouri&polygon_geojson=1&format=geojson')
   .then(function(response) {
     return response.json();
   })
@@ -47,19 +51,28 @@ fetch('platte-county.geojson')
 
     // Add the county layer to the map
     countyLayer.addTo(map);
+
+    // Calculate the bounds of the county layer
+    var countyBounds = countyLayer.getBounds();
+
+    // Create a new Graticule instance with the desired options
+    var graticule = L.graticule({
+        interval: 2,
+        style: {
+            color: '#333',
+            weight: 1,
+            opacity: 0.5
+        }
+    }).addTo(map);
+    
+    graticule.bringToFront();
+
+
+    // Fit the map to the bounds of the county layer
+    map.fitBounds(countyBounds);
   });
 
-    // Create a new map instance
-   // var map = L.map('map');
 
-    // Set the map view to Platte County
-    //map.setView([39.3621, -94.7722], 10);
-
-    // Add the OpenStreetMap tile layer to the map
-    //L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      //attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-      //maxZoom: 18
-    //}).addTo(map);
   </script>
 </body>
 </html>
