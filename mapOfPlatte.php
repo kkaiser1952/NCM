14c2c83fb68595f23a2ff1dfcc2a56ac8444e4e1
root <root@net-control.us>
2023-04-11 19:00:01 +0000

    Sync from production 2023-04-11 19:00:01.822348185+00:00

--- mapOfPlatte.php
+++ mapOfPlatte.php
@@ -4,14 +4,14 @@
   <title>Leaflet Map Example</title>
   
   <!-- Include Leaflet CSS -->
-  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
-  
-  <!-- Include Leaflet JavaScript -->
-  <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
+  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.2/dist/leaflet.css" integrity="sha256-sA+zWATbFveLLNqWO2gtiw3HL/lh1giY/Inf1BJ0z14=" crossorigin="" />
+     <script src="https://unpkg.com/leaflet@1.9.2/dist/leaflet.js" integrity="sha256-o9N1jGDZrf5tS+Ft4gbIK7mYMipq9lqpVJ91xHSyKhg=" crossorigin=""></script>
   
   <style>
-    #map {
-      height: 500px;
+    html, body, #map {
+      height: 100%;
+      margin: 0;
+      padding: 0;
     }
   </style>
 </head>
@@ -22,39 +22,14 @@
     // Create a new map instance
     var map = L.map('map');
 
-    // Set the map view to the center of the two counties
-    var latlng = L.latLng(39.3621, -94.7722);
-    map.setView(latlng, 10);
+    // Set the map view to Clay and Platte Counties
+    map.fitBounds([[39.165, -94.8335], [39.4385, -94.499]]);
 
     // Add the OpenStreetMap tile layer to the map
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
       maxZoom: 18
     }).addTo(map);
-
-    // Load the GeoJSON data for Clay County from OpenStreetMap
-    fetch('https://nominatim.openstreetmap.org/search.php?q=Clay+County+Missouri&polygon_geojson=1&format=geojson')
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(data) {
-        // Create a new GeoJSON layer with the county boundary data
-        var countyLayer = L.geoJSON(data, {
-          style: {
-            color: 'blue',
-            fillOpacity: 0
-          }
-        });
-
-        // Add the county layer to the map
-        countyLayer.addTo(map);
-
-        // Calculate the bounds of the county layer
-        var countyBounds = countyLayer.getBounds();
-
-        // Fit the map to the bounds of the county layer
-        map.fitBounds(countyBounds);
-      });
 
     // Load the GeoJSON data for Platte County from OpenStreetMap
     fetch('https://nominatim.openstreetmap.org/search.php?q=Platte+County+Missouri&polygon_geojson=1&format=geojson')
@@ -66,19 +41,66 @@
         var countyLayer = L.geoJSON(data, {
           style: {
             color: 'blue',
-            fillOpacity: 0
+            fillOpacity: 0,
+            weight: 2
+          },
+          onEachFeature: function(feature, layer) {
+            layer.bindTooltip(feature.properties.display_name, {
+              sticky: true
+            });
           }
         });
 
         // Add the county layer to the map
         countyLayer.addTo(map);
-
-        // Calculate the bounds of the county layer
-        var countyBounds = countyLayer.getBounds();
-
-        // Fit the map to the bounds of the county layer
-        map.fitBounds(countyBounds);
       });
+
+    // Load the GeoJSON data for Clay County from OpenStreetMap
+    fetch('https://nominatim.openstreetmap.org/search.php?q=Clay+County+Missouri&polygon_geojson=1&format=geojson')
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        // Create a new GeoJSON layer with the county boundary data
+        var countyLayer = L.geoJSON(data, {
+          style: {
+            color: 'blue',
+            fillOpacity: 0,
+            weight: 2
+          },
+          onEachFeature: function(feature, layer) {
+            layer.bindTooltip(feature.properties.display_name, {
+              sticky: true
+            });
+          }
+        });
+
+        // Add the county layer to the map
+        countyLayer.addTo(map);
+      });
+      
+      // Add 5 randomly located numbered markers
+    for (var i = 1; i <= 5; i++) {
+      var marker = L.marker(getRandomCoords()).addTo(map);
+      marker.bindPopup('Marker ' + i).openPopup();
+      marker.bindTooltip(i.toString(), {
+        permanent: true,
+        direction: 'top',
+        className: 'numbered-marker'
+      });
+    }
+    
+    
+// Function to generate random coordinates within the map bounds
+function getRandomCoords() {
+  var southWest = map.getBounds().getSouthWest();
+  var northEast = map.getBounds().getNorthEast();
+  var lngSpan = northEast.lng - southWest.lng;
+  var latSpan = northEast.lat - southWest.lat;
+  var randomLng = Math.random() * lngSpan + southWest.lng;
+  var randomLat = Math.random() * latSpan + southWest.lat;
+  return [randomLat, randomLng];
+}
   </script>
 </body>
 </html>
