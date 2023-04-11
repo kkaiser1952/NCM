b2f6850aeb5c0a5620f13ddfb48de7920007f4f2
root <root@net-control.us>
2023-04-11 16:00:01 +0000

    Sync from production 2023-04-11 16:00:01.844380993+00:00

--- mapOfPlatte.php
+++ mapOfPlatte.php
@@ -1,16 +1,7 @@
 <!DOCTYPE html>
-<!-- Just a test map of Platte and Clay counties -->
-<!-- Suggested by ChatGPT -->
-<!-- map.setView([39.3172, -94.5123], 10); -->
-
-
-
 <html>
 <head>
-  <title>Leaflet Lat/Lon Graticule Demo</title>
-	<meta charset="utf-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <title>Leaflet Map Example</title>
   
   <!-- Include Leaflet CSS -->
   <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
@@ -18,11 +9,9 @@
   <!-- Include Leaflet JavaScript -->
   <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
   
-  <script src="js/Leaflet.Graticule.js"></script>
-  
   <style>
     #map {
-      height: 800px;
+      height: 600px;
     }
   </style>
 </head>
@@ -33,8 +22,8 @@
     // Create a new map instance
     var map = L.map('map');
 
-    // Set the map view to Platte County
-    map.setView([39.3172, -94.5123], 10);
+    // Set the map view to Missouri
+    map.setView([38.5, -92.5], 7);
 
     // Add the OpenStreetMap tile layer to the map
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -42,71 +31,45 @@
       maxZoom: 18
     }).addTo(map);
 
-    // Load the GeoJSON data for Platte County from OpenStreetMap
-    fetch('https://nominatim.openstreetmap.org/search.php?q=Platte+County+Missouri&polygon_geojson=1&format=geojson')
-      .then(function(response) {
+    // Load the GeoJSON data for Clay and Platte Counties from OpenStreetMap
+    Promise.all([
+      fetch('https://nominatim.openstreetmap.org/search.php?q=Clay+County+Missouri&polygon_geojson=1&format=geojson'),
+      fetch('https://nominatim.openstreetmap.org/search.php?q=Platte+County+Missouri&polygon_geojson=1&format=geojson')
+    ])
+    .then(function(responses) {
+      return Promise.all(responses.map(function(response) {
         return response.json();
-      })
-      .then(function(data) {
-        // Create a new GeoJSON layer with the county boundary data
-        var countyLayer = L.geoJSON(data);
+      }));
+    })
+    .then(function(datas) {
+      // Create new GeoJSON layers with the county boundary data
+      var clayLayer = L.geoJSON(datas[0]);
+      var platteLayer = L.geoJSON(datas[1]);
 
-        // Add the county layer to the map
-        countyLayer.addTo(map);
+      // Add the county layers to the map
+      clayLayer.addTo(map);
+      platteLayer.addTo(map);
 
-        // Calculate the bounds of the county layer
-        var countyBounds = countyLayer.getBounds();
+      // Calculate the bounds of the county layers
+      var clayBounds = clayLayer.getBounds();
+      var platteBounds = platteLayer.getBounds();
 
-        // Fit the map to the bounds of the county layer
-        map.fitBounds(countyBounds);
-      });
-      
-    // Load the GeoJSON data for Clay County from OpenStreetMap
-    fetch('https://nominatim.openstreetmap.org/search.php?q=Clay+County+Missouri&polygon_geojson=1&format=geojson')
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(data) {
-        // Create a new GeoJSON layer with the county boundary data
-        var countyLayer = L.geoJSON(data);
+      // Fit the map to the bounds of the county layers
+      map.fitBounds(clayBounds.extend(platteBounds));
 
-        // Add the county layer to the map
-        countyLayer.addTo(map);
+      // Add tooltips to the county layers
+      clayLayer.bindTooltip('Clay County').openTooltip();
+      platteLayer.bindTooltip('Platte County').openTooltip();
 
-        // Calculate the bounds of the county layer
-        var countyBounds = countyLayer.getBounds();
-
-        // Fit the map to the bounds of the county layer
-        map.fitBounds(countyBounds);
-      });
-      
-
-    // Create a new Graticule instance with the desired options
-/*    
-    var graticule = L.latlngGraticule({
-      interval: 2,
-      showLabel: true,
-      opacity: 0.5,
-      weight: 0.8,
-      color: '#000000',
-      fontColor: '#333',
-      zoomInterval: [
-            {start: 2, end: 3, interval: 30},
-            {start: 4, end: 4, interval: 10},
-            {start: 5, end: 7, interval: 5},
-            {start: 8, end: 10, interval: 1}
-        ]
-    }).addTo(map);
-    
-    console.log(map.options.crs);
-
-    // Add the Graticule layer to the map
-    //graticule.addTo(map);
-
-    // Bring the Graticule layer to the front of the map
-    //graticule.bringToFront();
-*/
+      // Add markers to the map with random coordinates and numbers
+      for (var i = 1; i <= 5; i++) {
+        var marker = L.marker([
+          Math.random() * (platteBounds.getNorth() - platteBounds.getSouth()) + platteBounds.getSouth(),
+          Math.random() * (platteBounds.getEast() - platteBounds.getWest()) + platteBounds.getWest()
+        ]).addTo(map);
+        marker.bindPopup('Marker ' + i);
+      }
+    });
   </script>
 </body>
 </html>
-
