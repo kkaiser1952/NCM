79c5a4af2320c2c1a68684334b21d249dc53120e
root <root@net-control.us>
2023-04-30 22:00:01 +0000

    Sync from production 2023-04-30 22:00:01.284820172+00:00

--- mapOfPlatte3.php
+++ mapOfPlatte3.php
@@ -11,7 +11,7 @@
   
 <!-- Include Graticule plugin JavaScript -->
 <!--<script src="https://ha8tks.github.io/Leaflet.Maidenhead/src/L.Maidenhead.js"></script>-->
-<script src="js/maidenhead8.js"></script>
+<!--<script src="js/maidenhead8.js"></script>-->
  
 
   <style>
@@ -100,6 +100,106 @@
         countyLayer.addTo(map);
       });
       
+      // Add a Maidenhead grid overlay with 4 levels of precision
+      var gridLevels = [2, 4, 6, 8]; // Define the levels of precision
+      var gridPrecisions = gridLevels.map(level => Math.pow(10, -level / 2)); // Calculate the grid precisions
+      var minLat = -90;
+      var maxLat = 90;
+      var minLng = -180;
+      var maxLng = 180;
+
+      for (var i = 0; i < gridLevels.length; i++) {
+        var level = gridLevels[i];
+        var precision = gridPrecisions[i];
+        var lat = minLat;
+        while (lat < maxLat) {
+          var lng = minLng;
+          while (lng < maxLng) {
+            var maidenhead = toMaidenhead(lat, lng, level);
+            var label = toMaidenheadLabel(maidenhead, level);
+            var bounds = [[lat, lng], [lat + precision, lng + precision]];
+            L.rectangle(bounds, { color: 'blue', weight: 1, fillOpacity: 0, attribution: label }).addTo(map);
+            lng += precision;
+          }
+          lat += precision;
+        }
+      }
+      
+      L.rectangle(bounds, {color: 'blue', weight: 2, fillOpacity: 0, attribution: label }).addTo(map);
+      
+      function toMaidenhead(lat, lng, level) {
+          var maidenhead = '';
+          var latCoord = (lat + 90) / 180;
+          var lngCoord = (lng + 180) / 360;
+          var latDiv = 1;
+          var lngDiv = 1;
+        
+          for (var i = 0; i < level; i++) {
+            latDiv *= 2;
+            lngDiv *= 2;
+            var latIndex = Math.floor(latCoord * latDiv);
+            var lngIndex = Math.floor(lngCoord * lngDiv);
+            maidenhead += String.fromCharCode(65 + (lngIndex * 2) + (latIndex % 2));
+            maidenhead += String.fromCharCode(65 + (latIndex * 1) + ((lngIndex % 2) * 24));
+            latCoord -= latIndex / latDiv;
+            lngCoord -= lngIndex / lngDiv;
+          }
+        
+          return maidenhead;
+        }
+
+    function toMaidenheadLabel(lat, lon, precision) {
+      //const coords = toMaidenhead(lat, lon, precision);
+      //const labelLat = `${Math.abs(lat).toFixed(precision)}${lat >= 0 ? 'N' : 'S'}`;
+      //const labelLon = `${Math.abs(lon).toFixed(precision)}${lon >= 0 ? 'E' : 'W'}`;
+      //return `${coords} (${labelLat}, ${labelLon})`;
+    }
+
+/* 
+      // Convert latitude and longitude to Maidenhead grid square
+      function toMaidenhead(lat, lng, level) {
+        var precision = Math.pow(10, -level / 2);
+        var latIndex = Math.floor((lat + 90) / precision);
+        var lngIndex = Math.floor((lng + 180) / (2 * precision));
+        var latSubIndex = Math.floor(((lat + 90) % precision) / (precision / 10));
+        var lngSubIndex = Math.floor(((lng + 180) % (2 * precision)) / (precision / 5));
+        return String.fromCharCode(65 + lngIndex) +
+               String.fromCharCode(65 + latIndex) +
+               latSubIndex.toString() +
+               lngSubIndex.toString();
+      }
+      
+      // Convert Maidenhead grid square to label
+      function toMaidenheadLabel(maidenhead, level) {
+        return 'Maidenhead: ' + maidenhead + ', Precision: ' + level.toString();
+      }
+     
+      // Add a Maidenhead grid overlay with 4 levels of precision
+     
+    var gridLevels = [2, 4, 6, 8]; // Define the levels of precision
+    var gridPrecisions = gridLevels.map(level => Math.pow(10, -level / 2)); // Calculate the grid precisions
+    var minLat = 35.5;
+    var maxLat = 40.5;
+    var minLng = -95.5;
+    var maxLng = -93.5;
+    
+    for (var i = 0; i < gridLevels.length; i++) {
+      var level = gridLevels[i];
+      var precision = gridPrecisions[i];
+      var lat = minLat;
+      while (lat < maxLat) {
+        var lng = minLng;
+        while (lng < maxLng) {
+          var maidenhead = toMaidenhead(lat, lng, level);
+          var label = toMaidenheadLabel1(maidenhead, level);
+          var bounds = [[lat, lng], [lat + precision, lng + 2 * precision]];
+          L.rectangle(bounds, { color: 'blue', weight: 1, fillOpacity: 0, attribution: label }).addTo(map);
+          lng += 2 * precision;
+        }
+        lat += precision;
+      }
+    }
+      */
   
   </script>
 </body>
