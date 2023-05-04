b37eb070ed941b87d8e6bf80b597fa960b9f14b0
root <root@net-control.us>
2023-05-04 16:00:01 +0000

    Sync from production 2023-05-04 16:00:01.956763143+00:00

--- aprsFIapi.php
+++ aprsFIapi.php
@@ -1,9 +1,21 @@
 <?php
+function formatUnixTimestamp($unixTimestamp) {
+    $dateObject = new DateTime();
+    $dateObject->setTimestamp($unixTimestamp);
+
+    $date = $dateObject->format('m/d/Y');
+    $time = $dateObject->format('h:i:s A');
+
+    $datetime = "$date $time";
+
+    return $datetime;
+}
+
 
 // Set the API endpoint and parameters
 $url = 'http://api.aprs.fi/api/get';
 $params = array(
-    'name' => 'WA0TJT-1',
+    'name' => 'WA0TJT-8',
     'what' => 'loc',
     'apikey' => '5275.AYRjLwAFgx6ud',
     'format' => 'json'
@@ -33,8 +45,24 @@ curl_close($ch);
 $data = json_decode($response, true);
 
 // Output the latitude and longitude data
+
 $lat = $data['entries'][0]['lat'];
 $lng = $data['entries'][0]['lng'];
-echo "Latitude: $lat, Longitude: $lng";
+$time = $data['entries'][0]['time'];
+$lasttime = $data['entries'][0]['lasttime'];
+$altitude = $data['entries'][0]['altitude'];
+
+$current = formatUnixTimestamp($time);
+$lasttime = formatUnixTimestamp($lasttime);
+
+echo "Latitude: $lat, Longitude: $lng <br>";
+echo "Time: $time, Last Time: $lasttime <br>";
+echo "Altitude: $altitude <br>";
+echo "Current Time: $current <br>";
+echo "Last Time: $lasttime <br>";
+
+echo '<pre>'; print_r($data); echo '</pre>';
+
+
 
 ?>
