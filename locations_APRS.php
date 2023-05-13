825b35f418c2cc84e06cfd53b9700888d940d5e7
root <root@net-control.us>
2023-05-13 22:00:02 +0000

    Sync from production 2023-05-13 22:00:02.055612251+00:00

--- locations_APRS.php
+++ locations_APRS.php
@@ -155,7 +155,7 @@ echo $json;
        //echo "\n\n";
        
        //$deltaX = 'LOC&#916;';
-       $deltax = 'LOC&#916:APRS '.$objName.': Changed lat/lng, grid, w3w.  '.$aprs_comment.'; '.$thislatlng;
+       $deltax = 'LOC&#916:APRS '.$objName.': '.$aprs_comment.'; '.$thislatlng;
        $sql = 
        "INSERT INTO TimeLog 
             (timestamp, callsign, comment, netID)
