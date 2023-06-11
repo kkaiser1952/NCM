5f4f0d72543ed64cb4671a74a7db1a6cecbbaf12
root <root@net-control.us>
2023-06-11 17:00:01 +0000

    Sync from production 2023-06-11 17:00:01.748333005+00:00

--- getListOfFacilities.php
+++ getListOfFacilities.php
@@ -2,29 +2,35 @@
 // getListOfFacilities.php
 // This query gets and formats the selection criteria for the facility column 
 // Its called by CellEdityFunctions.js
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once "dbConnectDtls.php";
 
-	$recordID   = $_GET['recordID'];	
+	//$recordID   = $_GET['recordID'];	
 	
-	//$recordID = 93882;
+	$recordID = 134912;
 	
-	$sql = "SELECT netcall FROM NetLog WHERE recordID = $recordID LIMIT 0,1";
-	//echo "$sql";
+	$sql = "SELECT netcall FROM NetLog WHERE recordID = $recordID LIMIT 0,1;";
+	//echo "first sql: $sql <br>";
 	    $stmt = $db_found->prepare($sql);
 		$stmt -> execute();
 		$groupcall = $stmt->fetchColumn(0);
 		
-		//echo "$groupcall";
+		//echo "group call: $groupcall <br>";
 	 
-	$sql = "SELECT facility
-			  FROM `facility`
-			 WHERE groupcall = '$groupcall'
+	$sql = "SELECT `facility`
+			  FROM ncm.facility
+			 WHERE groupcall = '$groupcall';
 		   ";
 	//echo "$sql<br><br>";
-        $list = "{\" \":\" \","; // add a blank to the list
+        //$list = "{\" \":\" \","; // add a blank to the list
+        $list = "{";
         $listarray = array();
 	  	foreach($db_found->query($sql) as $row) {
-		++$num_rows;    /* "Traffic":"Traffic" */
+		//++$num_rows;    /* "Traffic":"Traffic" */
 		   // $list .= "$row[facility],";
 		   // $listarray[] = $row;
 		    $list .= "\"$row[facility]\":\"$row[facility]\",";
