f6a6484065ab0643c75a45090aab2796418530af
root <root@net-control.us>
2023-07-05 23:00:01 +0000

    Sync from production 2023-07-05 23:00:01.857995205+00:00

--- buildDailyReport2.php
+++ buildDailyReport2.php
@@ -6,144 +6,80 @@ error_reporting(E_ALL ^ E_NOTICE);
 
 require_once "dbConnectDtls.php";  // Access to MySQL
 
-// Your SQL query
-$sql = $db_found->prepare("
-    SELECT 
-        DATE(dttm) AS date, 
-        GROUP_CONCAT(DISTINCT netcall) AS netcall,
-        GROUP_CONCAT(netID) AS netIDs,
-        GROUP_CONCAT(callsign) AS callsign_list,
-        COUNT(DISTINCT netID) AS netID_count,
-        DATE_FORMAT(MAX(dttm), '%Y-%m-%d %W') AS dttm
-    FROM NetLog
-    WHERE dttm >= DATE_SUB(CURDATE(), INTERVAL 5 DAY)
-    GROUP BY date
-    ORDER BY date DESC
-");
+try {
 
+    // SQL query
+    $sql = "
+        SELECT
+          DATE_FORMAT(n.logdate, '%Y-%m-%d') AS logdate,
+          n.netID,
+          n.netcall,
+          COUNT(n.callsign) AS call_count,
+          TIME_FORMAT(TIMEDIFF(MAX(n.logclosedtime), MIN(n.logdate)), '%H:%i') AS TOD,
+          DATE_FORMAT(n.logdate, '%W') AS dow,
+          CASE
+            WHEN n.logclosedtime IS NULL OR n.logclosedtime = '0000-00-00 00:00:00' THEN 'open'
+            WHEN COUNT(n.callsign) < 2 THEN 'one checkin'
+            ELSE 'closed'
+          END AS status
+        FROM
+          NetLog n
+        WHERE
+          n.logdate >= DATE_SUB(CURDATE(), INTERVAL 5 DAY)
+        GROUP BY
+          DATE(n.logdate), n.netID";
 
+    // Prepare and execute the query
+    $stmt = $db_found->prepare($sql);
+    $stmt->execute();
 
-// Execute the SQL query and store the result in $result variable
-$sql->execute();
-$result = $sql->fetchAll(PDO::FETCH_ASSOC);
-
-// CSS styles for the report table
-$cssStyles = "
-<style>
-    table {
-        border-collapse: collapse;
-    }
-    
-    th, td {
-        padding: 8px;
-        border: 1px solid #000000;
-    }
-    
-    .odd-row {
-        background-color: #F0F0F0;
-    }
-    
-    .even-row {
-        background-color: #FFFFFF;
-    }
-    
-    .red-row {
-        background-color: #FF0000;
-        color: white;
-        font-weight: bold;
-    }
-    
-    .red-bg {
-        background-color: red;
-        color: white;
-    }
-    
-    .blue-bg {
-        background-color: blue;
-        color: white;
-        font-weight: bold;
-    }
-    
-    label {
-        font-weight: bold;
-    }
-    
-    .date-row {
-        font-weight: bold;
-        font-size: 14pt;
-        color: green;
-</style>
-";
-
-// Print CSS styles
-echo $cssStyles;
-
-// Print the title
-if (!empty($result)) {
-    $title = "Past 5 days NCM Report";
-    echo '<h1>' . $title . '</h1>
-    
-     <form>  <!-- This adds a legend to the top of the report -->
-        <label for="open_nets">Open Net:</label>
-        <input type="text" id="open_nets" name="open_nets" class="red-bg" value="">
-        
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-
-        <label for="one_entry">One Entry:</label>
-        <input type="text" id="one_entry" name="one_entry" class="blue-bg" value=""><br><br>
-      </form>
-    ';
-} else {
-    echo 'No results found.';
-}
-
-// Check if there are any rows in the result set
-if (!empty($result)) {
-    // Start the table
+    // HTML table generation
     echo '<table>';
 
-    // Table header
-    echo '<tr>';
-    echo '<th>netID</th>';
-    echo '<th>dttm</th>';
-    echo '<th>netcall</th>';
-    echo '<th>count</th>';
-    echo '</tr>';
+    // Variable to keep track of the current logdate
+    $currentLogdate = '';
 
-    // Table rows
-    foreach ($result as $row) {
-        // Output the summary row for each day
-        echo '<tr class="date-row">';
-        echo '<td>' . $row['netID_count'] . '</td>';
-        echo '<td>' . $row['dttm'] . '</td>';
-        echo '<td></td>';
-        echo '<td>' . $row['count'] . '</td>';
-        echo '</tr>';
-
-        // Fetch and output the detailed rows for each net and call
-        $date = $row['date'];
-        $sqlDetails = $db_found->prepare("
-            SELECT netID, dttm, netcall, `count`
-            FROM NetLog
-            WHERE DATE(dttm) = :date
-            ORDER BY dttm
-        ");
-        $sqlDetails->execute(array(':date' => $date));
-        $detailsResult = $sqlDetails->fetchAll(PDO::FETCH_ASSOC);
-
-        foreach ($detailsResult as $detailRow) {
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        // Check if the logdate has changed
+        if ($currentLogdate != $row['logdate']) {
+            // Display the new logdate group header
             echo '<tr>';
-            echo '<td>' . $detailRow['netID'] . '</td>';
-            echo '<td>' . $detailRow['dttm'] . '</td>';
-            echo '<td>' . $detailRow['netcall'] . '</td>';
-            echo '<td>' . $detailRow['count'] . '</td>';
+            echo '<th>' . $row['logdate'] . ' (' . $row['dow'] . ')</th>';
+            echo '<th>' . $row['call_count'] . '</th>';
             echo '</tr>';
+
+            // Update the current logdate
+            $currentLogdate = $row['logdate'];
         }
+
+        // Determine the row background color and text color based on the status
+        $rowBgColor = '';
+        $textColor = '';
+        $fontSize = '12px';
+
+        if ($row['status'] == 'open') {
+            $rowBgColor = 'red';
+            $textColor = 'white';
+        } elseif ($row['status'] == 'one checkin') {
+            $rowBgColor = 'blue';
+            $textColor = 'white';
+        }
+
+        // Display the row with the appropriate styling
+        echo '<tr style="background-color: ' . $rowBgColor . '; color: ' . $textColor . ' font-size: ' . $fontSize . ';">';
+        echo '<td>' . $row['netID'] . '</td>';
+        echo '<td>' . $row['netcall'] . '</td>';
+        echo '<td>' . $row['call_count'] . '</td>';
+        echo '<td>' . $row['TOD'] . '</td>';
+        echo '</tr>';
     }
 
-    // End the table
     echo '</table>';
-} else {
-    echo 'No results found.';
+} catch (PDOException $e) {
+    // Handle any errors
+    echo 'Error: ' . $e->getMessage();
 }
+
+// Close the database connection
+$dbh = null;
 ?>
