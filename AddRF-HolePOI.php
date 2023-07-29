93e577f44cb6f087ea5529ff9dc7b12425c4102e
root <root@net-control.us>
2023-07-29 21:00:01 +0000

    Sync from production 2023-07-29 21:00:01.742462160+00:00

--- AddRF-HolePOI.php
+++ AddRF-HolePOI.php
@@ -1,59 +1,65 @@
 <!doctype html>
 
 <?php
+
 ini_set('display_errors', 1);
 error_reporting(E_ALL ^ E_NOTICE);
 
 require_once "dbConnectDtls.php";
 
-// Initialize variables
-$callsign = $radius = $w3w = $type = $notes = "";
-
-// Check if the form was submitted
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
+// Check if the form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Get the values from the form submission
-    $callsign = $_POST['callsign'];
+    $callsign = strtoupper($_POST['callsign']); // Convert callsign to uppercase
     $radius = $_POST['radius'];
     $w3w = $_POST['w3w'];
     $type = $_POST['type'];
-    $notes = $_POST['notes'];
 
-    // Set hard-coded values
-    $class = 'RF-Hole';
-    $Notes = 'date added';
+    // Set the current date (date only) in the 'Notes' column
+    $currentDate = date('Y-m-d');
+    $notes = "Created: " . $currentDate;
 
-    // Generate the 'name' value by concatenating 'RF-HoleK' with the last inserted ID
-    $name = 'RF-HoleK' . $conn->lastInsertId();
+    // Concatenate "RF-HoleK1" with the ID number
+    $namePrefix = "RF-HoleK1"; // Change this if you want a different prefix
 
-    // Prepare and execute the SQL statement to insert the data
-    $stmt = $conn->prepare("INSERT INTO poi (callsign, radius, w3w, type, class, Notes, name) VALUES (:callsign, :radius, :w3w, :type, :class, :Notes, :name)");
-    $stmt->bindParam(":callsign", $callsign);
-    $stmt->bindParam(":radius", $radius);
-    $stmt->bindParam(":w3w", $w3w);
-    $stmt->bindParam(":type", $type);
-    $stmt->bindParam(":class", $class);
-    $stmt->bindParam(":Notes", $Notes);
-    $stmt->bindParam(":name", $name);
+    // Prepare and bind the SQL statement to insert the data
+    $stmt = $db_found->prepare("INSERT INTO poi (callsign, radius, w3w, type, Notes, name) VALUES (:callsign, :radius, :w3w, :type, :notes, :name)");
+
+    // Bind the values to the named placeholders
+    $stmt->bindValue(':callsign', $callsign);
+    $stmt->bindValue(':radius', $radius);
+    $stmt->bindValue(':w3w', $w3w);
+    $stmt->bindValue(':type', $type);
+    $stmt->bindValue(':notes', $notes);
+
+    // Concatenate "RF-HoleK1" with the ID number
+    $name = $namePrefix . ' ' . $db_found->lastInsertId();
+    $stmt->bindValue(':name', $name);
 
     // Execute the prepared statement
     if ($stmt->execute()) {
         // Retrieve the entire record by querying the database
-        $query = "SELECT * FROM poi WHERE id = :id";
-        $stmt = $conn->prepare($query);
-        $stmt->bindParam(":id", $conn->lastInsertId());
-        $stmt->execute();
-        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        $query = "SELECT * FROM poi WHERE id = " . $db_found->lastInsertId();
+        $result = $db_found->query($query);
 
-        echo json_encode($row); // Return the inserted record's data as JSON
+        if ($result && $result->rowCount() > 0) {
+            $row = $result->fetch(PDO::FETCH_ASSOC);
+            echo json_encode($row); // Return the inserted record's data as JSON
+        } else {
+            echo "Error: Record not found";
+        }
+
     } else {
-        echo "Error: Failed to insert data";
+        echo "Error: " . $stmt->errorInfo()[2];
     }
     exit; // End the script here, no need to execute the remaining code.
 }
+// AddRF-HolePOI.php code ends here
 ?>
-<!-- Rest of the HTML code as before -->
-     
+<!-- Your HTML form code here -->
 
+   
+<!-- ///slap.rider.steer -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
