70b3eb11de4958cf5581062112de3ea89716e8dc
root <root@net-control.us>
2023-07-31 05:00:02 +0000

    Sync from production 2023-07-31 05:00:02.105178168+00:00

--- map2.php
+++ map2.php
@@ -42,7 +42,7 @@
     	       
 	// Loads the programs that create the station, poi, and object markers
 	require_once "stationMarkers.php";
-    //require_once "poiMarker.php";    
+    //require_once "poiMarker2.php";    
     require_once "objMarkers.php"; 
 ?>
 
