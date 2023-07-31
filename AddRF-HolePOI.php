e507ff4b6a30eacb08e78e6db5576b553b6c8636
root <root@net-control.us>
2023-07-31 00:00:01 +0000

    Sync from production 2023-07-31 00:00:01.833905168+00:00

--- AddRF-HolePOI.php
+++ AddRF-HolePOI.php
@@ -1,4 +1,5 @@
-<!doctype html>
+<!-- This program is used to add a RF-Hole style POI to the poi table -->
+<!-- Written: 2023=07-30 by WA0TJT -->
 
 <!-- ///slap.rider.steer -->
 
@@ -7,22 +8,25 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL ^ E_NOTICE);
 
 require_once "dbConnectDtls.php";
+require_once "ENV_SETUP.php";      // API's
+require_once "GridSquare.php";
+
+$w3wApiKey = getenv('w3wapikey');
+
 
 // Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Get the values from the form submission
     $callsign = strtoupper($_POST['callsign']); // Convert callsign to uppercase
-    $radius = $_POST['radius'];
-    $w3w = $_POST['w3w'];
-    $type = $_POST['type'];
+    $radius = $_POST['radius']; // This is in miles, .05, .1, 1.0, 2.5, etc
+    $w3w = $_POST['w3w']; // Entered as the primary value of a location
+    $type = $_POST['type']; // Meant to be the severity of the RF Hole ... K0 to K4
     
-    //echo 'w3w at tope; ' . $w3w;
-
-    // Convert the w3w value into latitude and longitude
+    // Convert the w3w value into latitude and longitude with this W3W API
     $curl = curl_init();
 
     curl_setopt_array($curl, array(
-        CURLOPT_URL => "https://api.what3words.com/v3/convert-to-coordinates?key=5WHIM4GD&words=$w3w",
+        CURLOPT_URL => "https://api.what3words.com/v3/convert-to-coordinates?key=$w3wApiKey&words=$w3w",
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_ENCODING => "",
         CURLOPT_MAXREDIRS => 10,
@@ -40,27 +44,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "cURL Error #:" . $err;
     } else {
         $w3wLL = json_decode($response, true);
+        
+        // Print the ARRAY created above in something easy to read
+        // print_r($w3wLL);
 
         // Check if latitude and longitude are present
         if (isset($w3wLL['coordinates'])) {
             $latitude = $w3wLL['coordinates']['lat'];
             $longitude = $w3wLL['coordinates']['lng'];
             
-            $parts = explode(',', $w3wLL['nearestPlace']);
-            
             // Use explode to split the string by comma and get the first part
-            //$parts = explode(',', $nearest);
+            $parts = explode(',', $w3wLL['nearestPlace']);
             
             // Trim any leading or trailing whitespace from the first part
             $city = trim($parts[0]);
+            $country = $w3wLL['country'];
             
-            //$city = $w3wLL['nearestPlace'];
-            
-            //echo "$w3wLL['"
-
+            $grid   = gridsquare($latitude, $longitude);   
 
             // Prepare and bind the SQL statement to insert the data
-            $stmt = $db_found->prepare("INSERT INTO poi (callsign, radius, w3w, type, name, tactical, Notes, latitude, longitude, city) VALUES (:callsign, :radius, :w3w, :type, :name, :tactical, :notes, :latitude, :longitude, :city)");
+            $stmt = $db_found->prepare("INSERT INTO poi (callsign, radius, w3w, type, name, tactical, Notes, latitude, longitude, city, country, grid, class) VALUES (:callsign, :radius, :w3w, :type, :name, :tactical, :notes, :latitude, :longitude, :city, :country, :grid, :class)");
 
             // Bind the values to the named placeholders
             $stmt->bindValue(':callsign', $callsign);
@@ -70,11 +73,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $stmt->bindValue(':latitude', $latitude);
             $stmt->bindValue(':longitude', $longitude);
             $stmt->bindValue(':city', $city);
+            $stmt->bindValue(':country', $country);
+            $stmt->bindValue(':grid', $grid);
+            $stmt->bindValue(':class', 'RF-Hole');
 
             // Set the current date (date only) in the 'Notes' column
             $currentDate = date('Y-m-d');
             $notes = isset($_POST['notes']) ? $_POST['notes'] : '';
-            $notesWithDate = "Created: " . $currentDate . ' ' . $notes;
+            $notesWithDate = "Created: " . $currentDate . ' -- ' . $notes;
 
             // Get the latest ID from the table 'poi'
             $query = "SELECT id FROM poi ORDER BY id DESC LIMIT 1";
@@ -88,23 +94,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             }
 
             // Calculate and bind the 'tactical' column value (e.g., "RF-HoleK1 535")
-            $namePrefix = "RF-HoleK1"; // Change this if you want a different prefix
+            $namePrefix     = "RF-HoleK1"; // Change this if you want a different prefix
             $tacticalPrefix = "RFH";
-            $tactical = $tacticalPrefix . '-' . $tacticalId;
-            $stmt->bindValue(':tactical', $tactical);
+            $tactical       = $tacticalPrefix . '-' . $tacticalId;
+                $stmt->bindValue(':tactical', $tactical);
 
-            $name = $namePrefix . ' ' . $tacticalId;
-            $stmt->bindValue(':name', $name);
+            $name           = $namePrefix . ' ' . $tacticalId;
+                $stmt->bindValue(':name', $name);
 
             // Bind the 'Notes' column value
-            $stmt->bindValue(':notes', $notesWithDate);
+                $stmt->bindValue(':notes', $notesWithDate);
 
             // Execute the prepared statement
             if ($stmt->execute()) {
                 // Retrieve the entire record by querying the database
                 $lastInsertId = $db_found->lastInsertId();
-                $query = "SELECT * FROM poi WHERE id = " . $lastInsertId;
-                $result = $db_found->query($query);
+                $query        = "SELECT * FROM poi WHERE id = " . $lastInsertId;
+                $result       = $db_found->query($query);
 
                 if ($result && $result->rowCount() > 0) {
                     $row = $result->fetch(PDO::FETCH_ASSOC);
@@ -123,10 +129,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     exit; // End the script here, no need to execute the remaining code.
 }
 ?>
-<!-- Your HTML form code here -->
-
    
-<!-- ///slap.rider.steer -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -190,13 +193,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         select[name="type"] option {
           font-size: 16pt; /* Adjust the font size as per your requirement */
         }
-
+        
     </style>
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
-    <script>
-    </script>
 
 </head>
 <body>
@@ -208,31 +208,33 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <!-- Container for the form (left-justified) -->
     <div class="form-container">
         <!-- ... -->
-<form id="poiForm" method="post">
-    <label for="callsign">Your Callsign:</label>
-    <input type="text" name="callsign" required><br>
-
-    <label for="radius">Radius in miles:</label>
-    <input type="text" name="radius" required><br>
-
-    <label for="w3w">What3Words:</label>
-    <input type="text" name="w3w" required><br>
-
-    <label for="type">Severity Type:</label>
-    <select name="type" required>
-        <option value="K1">K0 - No Copy</option>
-        <option value="K1">K1 - Very Poor</option>
-        <option value="K2">K2 - Noisy, Poor Copy</option>
-        <option value="K3">K3 - Noisy, Copyable</option>
-        <option value="K4">K4 - Variable</option>
-    </select><br>
-
-    <label for="notes">Notes:</label>
-    <input type="text" name="notes" id="infield" value="date added" onclick=empty(); required><br>
-
-    <input type="submit" value="Add Entry">
-    <input type="button" value="Cancel" onclick="resetForm()">
-</form>
+        <form id="poiForm" method="post">
+            <label for="callsign">Your Callsign:</label>
+            <input type="text" name="callsign" required><br>
+        
+            <label for="radius">Radius in miles, (.1 or .01, 1.2 etc):</label>
+            <input type="text" name="radius" required><br>
+        
+            <label for="w3w">What3Words:</label>
+            <input type="text" name="w3w" required><br>
+        
+            <label for="type">Severity Type:</label>
+            <select name="type" required>
+                <option value="" disabled>Click to choose</option>
+                <option value="K1">K0 - No Copy</option>
+                <option value="K1">K1 - Very Poor Copy</option>
+                <option value="K2">K2 - Noisy, Poor Copy</option>
+                <option value="K3">K3 - Noisy, Copyable</option>
+                <option value="K4">K4 - Variable</option>
+            </select><br>
+        
+            <label for="notes">Notes:</label>
+            <input type="text" name="notes" id="infield" value="" onclick=empty(); required><br>
+        
+            <input type="submit" value="Add Entry">
+            <input type="button" value="Cancel" onclick="resetForm()">
+        </form>
+    </div> <!-- End of div at class="form-container" -->
 
 <script>
     function resetForm() {
@@ -264,8 +266,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     inputElement.value = '';
   });
 </script>
-
-<!-- ... -->
 
 </body>
 </html>
