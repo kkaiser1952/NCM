b00266beffb1fd4db2f0cdc1365fd54a09f44e1f
root <root@net-control.us>
2023-08-07 21:00:01 +0000

    Sync from production 2023-08-07 21:00:01.209194104+00:00

--- rfholeReport.php
+++ rfholeReport.php
@@ -1,193 +1,239 @@
 <!doctype html>
-<!-- This is the ICS-309 report -->
+<!-- This is the RF Hole Report report -->
+<!-- 2023-08-07 -->
+<!-- RF Hole  poi's are not stored by netID so i will ask for a center point to give all POI's within 25 miles of that location -->
+
 <?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL ^ E_NOTICE);
 
-	ini_set('display_errors',1); 
-	error_reporting (E_ALL ^ E_NOTICE);
+require_once "dbConnectDtls.php";
+require_once "ENV_SETUP.php";      // API's
+require_once "GridSquare.php";
 
-    require_once "dbConnectDtls.php";
-    
-    $q = intval($_GET["NetID"]);  
-   // $q = 3080;
-    
-    // The below SQL is used to report the parent and child nets
-    $sql = "SELECT subNetOfID, 
-			       GROUP_CONCAT(DISTINCT netID SEPARATOR ', ')
-			  FROM NetLog
-			 WHERE subNetOfID = $q
-			 ORDER BY netID";
-			 
-	$stmt = $db_found->prepare($sql);
-	
-	//	$stmt -> execute();
-	//		$parent = $stmt->fetchColumn(0);
-		$stmt -> execute();
-			$children = $stmt->fetchColumn(1); 
-		
-	//	echo "p= $parent<br>";
-	//	echo "c= $children<br>";
-    
-    
-    $sql1 = ("SELECT min(a.logdate) AS minlog, 
-    				 DATE(min(a.logdate)) AS indate, 
-    				 TIME(min(a.logdate)) AS intime, 
-    				 DATE(max(a.timeout)) AS outdate, 
-    				 TIME(max(a.timeout)) AS outtime, 
-    				 a.activity, a.fname, a.lname, 
-    				 a.netcontrol, 
-    				 a.callsign, 
-    				 a.netcall,
-    				 b.kindofnet, b.box4, b.box5, a.subNetOfID,
-    				 a.frequency
-    	       FROM NetLog  as a
-    	       	   ,NetKind as b
-    	       WHERE a.netcall = b.call
-			     AND netID = $q 
-			     AND logdate = (SELECT min(logdate) 
-								  FROM NetLog 
-								  WHERE netID = $q )
-			");
-    			
-			
-    foreach($db_found->query($sql1) as $row) {
-	    
-	    $fname  = $row[fname];  $lname   = $row[lname]; 		$activity = $row[activity];
-	    $indate = $row[indate]; $outdate = $row[outdate];	$netcntl = $row[callsign];
-	    $intime = $row[intime]; $outtime = $row[outtime];	$name = $row[activity];
-	    $parent = $row[subNetOfID]; $freq = $row[frequency]; $netCall = $row[netcall];
-	    //$home	= $row[home];
-	    	if ($row[netcontrol] == "PRM") {$netcontrol = "Net Control Operator"; $netopener = $row[callsign];};
-    }
+$w3wApiKey = getenv('w3wapikey');
+
+// Check if the form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get the values from the form submission
+    $grid = $_POST['grid'];
+            
+    // Prepare and bind the SQL statement to insert the data
+    $stmt = $db_found->prepare("SELECT id, class, Type, name, city, latitude, longitude,
+                                       grid, tactical, Notes, radius, w3w, band
+                                  FROM poi
+                                 WHERE class = 'rfhole'
+                                   AND grid in( :grid )
+                                 ORDER BY band  
+                               ");
+            // Bind the values to the named placeholders            
+            $stmt->bindValue(':grid', $grid);
+
+            // Execute the prepared statement
+            if ($stmt->execute()) {
+                // Fetch all rows as an associative array
+                $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                
+                // Perform the foreach loop to process each row
+                foreach ($result as $row) {
+                    // Access individual columns using keys in the $row associative array
+                    $column1Value = $row['column1']; 
+                    $column2Value = $row['column2']; 
+                        // Do whatever you need to do with the retrieved data
+                        // For example, you could echo the values or perform further operations
+                        echo "Column 1: $column1Value, Column 2: $column2Value<br>";
+                    } // End foreach
+                } // End if stmt
+                  else {
+                    // Handle the case when the statement execution fails
+                    echo "Error executing the statement.";
+                  } // End else
+} // End if $_server 
 ?>
 
+   
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>ICS 309</title>
-    <meta name="author" content="Graham" />
-    <link rel="stylesheet" type="text/css" media="all" href="css/ics309.css">
+    <title>Report RF-Hole POI's</title>
+    <meta name="Keith Kaiser" content="Graham" />
     <link href='https://fonts.googleapis.com/css?family=Allerta' rel='stylesheet' type='text/css'>
-    
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script> 
-    
-    <script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
-	
-	</script>
-	
-	<style>
-		
-	</style>
-	
-    
+
+<style>
+body {
+    margin: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    font-size: 18pt;
+    background-color: #f7f7f7; /* Optional: Add a background color for better visibility */
+}
+
+.container {
+    width: 60%; /* Adjust the width as per your requirement */
+    max-width: 1000px; /* Add a max-width for larger screens */
+}
+
+/* Center the title and instructions */
+.title-container {
+    text-align: center;
+    font-size: 24px;
+    margin-bottom: 20px;
+}
+
+.instructions {
+    max-width: 100%; /* Allow instructions to take full width */
+    font-size: 14pt;
+    color: #5a8bff; /* Set the desired text color */
+    -webkit-text-fill-color: #5a8bff; /* For Safari */
+    -webkit-opacity: 1; /* For Safari */
+}
+
+/* Container for the form */
+.form-container {
+    text-align: left;
+}
+
+
+label {
+    display: block;
+    margin-bottom: 5px;
+}
+
+input[type="text"],
+select {
+    width: 100%;
+    padding: 8px;
+    margin-bottom: 10px;
+    font-size: inherit;
+}
+
+input[type="submit"] {
+    background-color: #4CAF50;
+    color: white;
+    padding: 10px 20px;
+    border: none;
+    cursor: pointer;
+    width: 100%;
+    font-size: inherit;
+    font-weight: bold;
+}
+
+input[type="submit"]:hover {
+    background-color: #45a049;
+}
+
+/* Style the select element */
+select[name="type"] {
+    font-size: 16pt; /* Adjust the font size as per your requirement */
+}
+
+/* Style the options within the dropdown */
+select[name="type"] option {
+    font-size: 16pt; /* Adjust the font size as per your requirement */
+}
+
+/* CSS to horizontally align checkboxes and match style with select */
+.checkbox-container {
+    display: inline-block;
+    margin-right: 20px;
+}
+
+.checkbox-container input[type="checkbox"] {
+    margin-right: 12px;
+}
+
+.checkbox-container label {
+    font-size: 16pt;
+    margin-right: 10px; /* Add more space between the label and checkbox */
+}
+
+/* Position the link on the right side and style it */
+.list-pois-link {
+    font-size: 18px;
+    float: right;
+    margin-top: 10px;
+    color: #5a8bff;
+}
+
+/* Add some spacing between the buttons and the link */
+#poiForm {
+    margin-bottom: 20px;
+}
+
+/* Center the form horizontally */
+.form-container {
+    margin: 0 auto;
+}
+
+        
+</style>
+
 </head>
 <body>
-	<div class = "row1">
-		<span class = "r1box1">COMM Log<br>ICS 309<br><?php echo "$netCall"; ?>
-		</span>
-		<span class = "r1box2">1. Incident Name:<?php echo "  Net#: $q<br>$activity"; ?>
-		</span>
-		<span class = "r1box3">2. Operational Period (Date/Time)<br>
-			From:<?php echo " $indate $intime<br>
-			To: $outdate $outtime"; ?>
-		</span>
-	</div>
-	
-	
-	<div class = "row2">
-		<span class = "r2box1">3. Radio Net Name or Position/Tactical Call
-		</span>
-		<span class = "r2box2">4. Radio Operator (Name, Call Sign)<br><?php echo "$netcntl - $fname $lname Net Control"; ?>
-		</span>
-	</div>
-	
-	<div class = "row3">
-		<span class = "r3box1">COMMUNICATIONS LOG
-		</span>
-	</div>
-	
-	<div class = "logtable">
-		<table class = "table1">
-			<thead id="thead" style="text-align: center;">			
-				<tr> 
-					<th class="th1" colspan="1">	Time<br>(Local)</th>
-					<th class="th2" colspan="2">FROM:<br>Call Sign/ID | Msg #</th>
-					<th class="th3" colspan="2">TO:<br>Call Sign/ID | Msg #</th>
-					<th class="th4" colspan="1">Message</th>
-				</tr>
-			</thead>
-			<tbody>
-				<?php
-	        
-			     /*   $sql = ("SELECT time(TIMESTAMP) as timestamp, 
-			                        ID, callsign, comment
-			        		   FROM TimeLog 
-			        		  WHERE netID = $q 
-			        		    AND comment <> 'Initial Log In'
-			        		    AND comment NOT LIKE 'this id was deleted'
-			        		  ORDER BY timestamp");
-			     */   		  
-                    $sql = ("SELECT time(TIMESTAMP) as timestamp, 
-			                        ID, callsign, comment, uniqueID
-			        		   FROM TimeLog 
-			        		  WHERE netID = $q
-			        		    AND comment <> 'Initial  Log In'
-			        		    AND comment NOT LIKE '%this id was deleted%'
-                                AND comment <> 'The log was closed, ICS-214 Created'
-                                AND callsign NOT IN('GENCOMM', 'weather')
-                                AND comment <> 'The log was re-opened'
-                                AND comment NOT LIKE '%Mode set to:%'
-                                AND comment NOT LIKE '%Opened the  net from%'
-			        		  ORDER BY timestamp");
-			/*
-			  
-    			  SELECT DISTINCT time(timestamp) as timestamp, band, comment,
-	   TimeLog.callsign
-  FROM TimeLog, NetLog
- WHERE TimeLog.netID = 1274 
-   AND NetLog.netID = 1274
-   AND TimeLog.id = NetLog.id
- ORDER BY timestamp
-    			  
-    			  
-    			         $sql = ("SELECT time(t1.timestamp) as timestamp, t1.callsign, t2.band, t1.comment, t1.netID, t1.id
-FROM TimeLog t1 
-JOIN (SELECT band, netID, id FROM NetLog WHERE netID = 1274) t2 
-ON (t1.id = t2.id ) 
-WHERE t1.netID=1274 AND t2.netID = 1274
-GROUP BY band, callsign, timestamp
-ORDER by timestamp"); */
-						foreach($db_found->query($sql) as $row) {
-							
-							echo "<tr style=\"height: 17pt\">
-									   <td class=\"box4td1\"  colspan=\"1\">	$row[timestamp]</td>
-									   <td class=\"box4td2\"  colspan=\"1\">	$row[callsign]</td>
-									   <td class=\"box4td3\"  colspan=\"1\">UNK</td>
-									   <td class=\"box4td4\"  colspan=\"1\">	NCO</td>
-									   <td class=\"box4td5\"  colspan=\"1\">$row[uniqueID]</td>
-									   <td class=\"box4td6\"  colspan=\"1\">	$row[comment]</td>
-								  </tr>";			
-					}
-				?>
-			</tbody>
-			<tfoot>
-			</tfoot>
-		</table>
-	</div>
-	
-	<div class = "row5">
-		<span class = "r5box1">6. Prepared By (Name, Call sign)<br><?php echo "$fname $lname -- $netcntl"; ?>
-		</span>
-		<span class = "r5box2">7. Date &amp; Time Prepared<br><?php echo date('l jS \of F Y h:i:s A'); ?>
-		</span>
-		<span class = "r5box3">8.<br>Page 1 of 
-		
-		</span>
-	</div>
-	
-	<p>ics309.php</p>
-	
-	
-	
+    <div class="container">
+    <!-- Container for the title (left-justified) -->
+    <div class="title-container">
+        <h2>Welcome to the RF Hole Report Program</h2>
+        <h3 id="instructions">Please fill out this form to submit the<br> grid square(s) (e.g. EM29, EM29qe) to <br>identify the location of RF Holes. <br>Seperate multiple grids with a comma.<br> Additional help can be found at;<br> <a href="https://net-control.us/help.php" target="_blank">https://net-control.us/help.php</a> </h3>
+    </div>
+    
+    <!-- Container for the form (left-justified) -->
+    <div class="form-container">
+        <!-- ... -->
+        <form id="poiForm" method="post">
+            <label for="callsign">Enter one or more maidenhead grid square(s):</label>
+            <input type="text" name="callsign" id="callsign" placeholder="(e.g., EM29, EM29qe, EM29qe78)" required>
+                
+            <input type="submit" id="submitBtn" value="Request Report">
+            <input type="button" value="Cancel" onclick="resetForm()">
+            
+            <!-- Link to List All PoIs -->
+            <a href="https://net-control.us/listAllPOIs.php" class="list-pois-link">List All PoI's</a>
+        </form>
+    </div> <!-- End of div at class="form-container" -->
+</div> <!-- End of div at class="container" -->
+    
+<script>
+
+</script>
+
+
+<script>
+    function resetForm() {
+        document.getElementById("poiForm").reset();
+    }
+
+    $(document).ready(function() {
+        $('#poiForm').submit(function(e) {
+            e.preventDefault();
+            $.ajax({
+                url: 'RF-HoleReport.php',
+                method: 'POST',
+                data: $(this).serialize(),
+                success: function(response) {
+                    alert(response);
+                    resetForm(); // Reset the form after successful submission
+                }
+            });
+        });
+    });
+</script>
+
+<script>
+  // Get the input element
+  const inputElement = document.getElementById('infield');
+
+  // Add a click event listener to the input field
+  inputElement.addEventListener('click', function() {
+    // Clear the value when clicked
+    inputElement.value = '';
+  });
+</script>
+
 </body>
 </html>
