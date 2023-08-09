2ce9263b0980f5c264e47e3b69bc205519a187ab
root <root@net-control.us>
2023-08-09 21:00:01 +0000

    Sync from production 2023-08-09 21:00:01.902166459+00:00

--- map.php
+++ map.php
@@ -61,8 +61,8 @@
 	<link rel="shortcut icon" type="image/x-icon" href="favicons/apple-icon.png">
 
      <!-- ******************************** Load LEAFLET from CDN *********************************** -->
-     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.2/dist/leaflet.css" integrity="sha256-sA+zWATbFveLLNqWO2gtiw3HL/lh1giY/Inf1BJ0z14=" crossorigin="" />
-     <script src="https://unpkg.com/leaflet@1.9.2/dist/leaflet.js" integrity="sha256-o9N1jGDZrf5tS+Ft4gbIK7mYMipq9lqpVJ91xHSyKhg=" crossorigin=""></script>
+     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
+     <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
      <!-- ********************************* End Load LEAFLET **************************************** -->
      
      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
