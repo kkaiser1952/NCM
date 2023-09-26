eccdbffa3e2d0052f399198c96d78a2deab0ade6
root <root@net-control.us>
2023-09-26 01:00:01 +0000

    Sync from production 2023-09-26 01:00:01.963625940+00:00

--- index_Proposed.php
+++ index_Proposed.php
@@ -51,7 +51,7 @@
 
     require_once "dbConnectDtls.php";  // Access to MySQL
     require_once "wx.php";			   // Makes the weather information available
-    require_once "NCMStats.php";       // Get some stats
+//    require_once "NCMStats.php";       // Get some stats
 ?>
 
 <html lang="en" >
