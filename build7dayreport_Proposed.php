9cec887bb6816b1a8831d52a6ccbe040201c9891
root <root@net-control.us>
2023-10-04 16:00:01 +0000

    Sync from production 2023-10-04 16:00:01.263833962+00:00

--- build7dayreport_Proposed.php
+++ build7dayreport_Proposed.php
@@ -396,49 +396,29 @@ ORDER BY `TimeLog`.`netID` ASC
 $sql = $db_found->prepare("
 SELECT 
     CASE WHEN nl.subNetOfID <> 0 THEN CONCAT(nl.subNetOfID, '/', nl.netID) 
-         ELSE nl.netID 
-    END AS netID,
-    CASE 
-        WHEN nl.logdate <> '0000-00-00 00:00:00' THEN nl.logdate
-        WHEN nl.timeout IS NULL THEN (
-            SELECT MAX(dttm) FROM NetLog WHERE dttm IS NOT NULL
-        )
-        ELSE nl.timeout
-    END AS logdate,
+            ELSE nl.netID END AS netID,
+    CASE WHEN nl.logdate <> '0000-00-00 00:00:00' THEN nl.logdate
+            ELSE (SELECT MAX(logdate) FROM NetLog) END AS logdate,
     nl.netcall,
     nl.stations,
     nl.pb,
     nl.testnet,
-    CASE WHEN nl.logclosedtime IS NULL THEN DATE_ADD(
-            (SELECT MAX(dttm) FROM NetLog WHERE dttm IS NOT NULL),
-            INTERVAL 30 MINUTE
-         )
-         WHEN nl.logclosedtime = '' THEN DATE_ADD(
-            (SELECT MAX(dttm) FROM NetLog WHERE dttm IS NOT NULL),
-            INTERVAL 30 MINUTE
-         )
-         ELSE nl.logclosedtime
-    END AS logclosedtime, 
-    CASE WHEN nl.pb = '0' THEN '' 
-         WHEN nl.pb = '1' THEN 'blue-bg' 
-         ELSE '' 
-    END AS PBcss,
+    CASE WHEN nl.logclosedtime IS NULL THEN DATE_ADD((SELECT max(dttm) FROM NetLog), INTERVAL 30 MINUTE)
+         WHEN nl.logclosedtime = '' THEN DATE_ADD((SELECT max(dttm) FROM NetLog), INTERVAL 30 MINUTE)
+            ELSE nl.logclosedtime END AS logclosedtime, 
+    CASE WHEN nl.pb = '0' THEN '' WHEN nl.pb = '1' THEN 'blue-bg' 
+            ELSE '' END AS PBcss,
     CASE WHEN nl.logclosedtime IS NOT NULL THEN '' 
          WHEN nl.logclosedtime IS NULL THEN 'green-bg' 
-         ELSE '' 
-    END AS LCTcss,
+            ELSE '' END AS LCTcss,
     CASE WHEN nl.netcall IN ('TEST', 'TE0ST', 'TEOST', 'TE0ST') OR nl.netcall LIKE '%test%' THEN 'purple-bg' 
-         ELSE '' 
-    END AS TNcss,
+            ELSE '' END AS TNcss,
     CASE WHEN nl.stations = 1 THEN 'red-bg' 
-         ELSE '' 
-    END AS CCss,
+            ELSE '' END AS CCss,
     CASE WHEN nl.facility <> '' THEN 'yellow-bg' 
-         ELSE '' 
-    END AS FNcss,
+            ELSE '' END as FNcss,
     CASE WHEN nl.subNetOfID <> 0 THEN 'cayenne-bg' 
-         ELSE '' 
-    END AS SNcss,
+            ELSE '' END AS SNcss,
     subquery.First_Login,
     (SELECT COUNT(DISTINCT netID) 
        FROM NetLog 
@@ -447,9 +427,7 @@ SELECT
     SEC_TO_TIME(SUM(
     CASE 
         WHEN nl.timeonduty IS NULL THEN 0
-        WHEN nl.logdate = '0000-00-00 00:00:00' THEN TIME_TO_SEC(
-            (SELECT MAX(dttm) FROM NetLog WHERE dttm IS NOT NULL)
-         )
+        WHEN nl.logdate = '0000-00-00 00:00:00' THEN TIME_TO_SEC((SELECT max(logdate) FROM NetLog))
         ELSE TIME_TO_SEC(nl.timeonduty)
     END
 )) AS Volunteer_Time,
@@ -457,9 +435,7 @@ SELECT
     SEC_TO_TIME(
     CASE 
         WHEN subquery.total_timeonduty_sum IS NULL THEN 0
-        WHEN nl.logdate = '0000-00-00 00:00:00' THEN TIME_TO_SEC(
-            (SELECT MAX(dttm) FROM NetLog WHERE dttm IS NOT NULL)
-         )
+        WHEN nl.logdate = '0000-00-00 00:00:00' THEN TIME_TO_SEC((SELECT max(logdate) FROM NetLog))
         ELSE subquery.total_timeonduty_sum
     END
 ) AS Total_Time,
@@ -474,10 +450,9 @@ SELECT
 FROM (
     SELECT netID, subNetOfID, 
     CASE WHEN logdate <> '0000-00-00 00:00:00' THEN logdate
-         ELSE NULL -- Set it to NULL when both logdate and timeout are NULL
-    END AS logdate, 
+            ELSE (SELECT MAX(logdate) FROM NetLog) END AS logdate, 
     netcall, COUNT(*) AS stations, 
-    pb, logclosedtime, testnet, timeonduty, facility, timeout -- Include 'timeout' in the subquery
+    pb, logclosedtime, testnet, timeonduty, facility
       FROM NetLog
      WHERE (DATE(logdate) >= DATE_SUB(CURDATE(), INTERVAL 7 DAY))
      GROUP BY netID
@@ -491,8 +466,6 @@ LEFT JOIN (
 LEFT JOIN TimeLog tl ON nl.netID = tl.netID
 GROUP BY netID
 ORDER BY CAST(nl.netID AS SIGNED) DESC;
-
-
 
 ");
 
