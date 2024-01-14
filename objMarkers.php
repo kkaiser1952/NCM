4336016465bd0993f5b10d706ccf93c7a6c2bafa
root <root@net-control.us>
2024-01-14 23:00:01 +0000

    Sync from production 2024-01-14 23:00:01.720334470+00:00

--- objMarkers.php
+++ objMarkers.php
@@ -1,7 +1,7 @@
 <?php
     
     ini_set('display_errors',1); 
-			//error_reporting (E_ALL ^ E_NOTICE);
+error_reporting (E_ALL ^ E_NOTICE);
 			
     // This is for what3words usage
     /* https://developer.what3words.com/public-api/docs#convert-to-3wa */
