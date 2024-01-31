a6df47530b767c495b094ef62b6b6f342126f81f
root <root@net-control.us>
2024-01-31 05:00:01 +0000

    Sync from production 2024-01-31 05:00:01.931921276+00:00

--- updateStationLocation.php
+++ updateStationLocation.php
@@ -13,7 +13,7 @@
     
     //$netID = intval( $_GET["NetID"] ); 
     //$cs = strtoupper('KC1TVI');  // does not return address1
-    $cs = 'kf0opa';
+    $cs = 'kf0ort';
  
 /*   
 st = stations table
