d127731f166aada843862d2abcb94fceafd5f937
root <root@net-control.us>
2024-02-06 18:00:02 +0000

    Sync from production 2024-02-06 18:00:02.045370176+00:00

--- index.php
+++ index.php
@@ -693,6 +693,8 @@
 <!-- My javascript -->	
 	<script src="js/NetManager.js"></script>         <!-- NCM Primary Javascrip 2018-1-18 -->
 	<script src="js/NetManager-p2.js"></script>	     <!-- Part 2 of NCM Primary Javascript 2018-1-18 -->
+	<script src="js/NetManager-W3W-APRS.js"></script> <!-- Created on 2024-02-06 for W3W & APRS --> 
+	
 	
 	<script src="js/CellEditFunction.js"></script>	 <!-- Added 2018-02-12 -->
 	
