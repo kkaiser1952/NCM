3d098cd9a77083f7137f5215c0714226ef3c713f
root <root@net-control.us>
2024-03-04 02:00:01 +0000

    Sync from production 2024-03-04 02:00:01.800896578+00:00

--- rowDefinitions.php
+++ rowDefinitions.php
@@ -215,11 +215,12 @@ echo ("
     
   
         <td $brbCols 
-            class=\"editable W3W  c24 cent\" 
+            class=\" W3W  c24 cent\" 
             id=\"w3w:$row[recordID]\" 
             oncontextmenu=\"mapWhat3Words('$row[w3w]');return false; \" 
-            onClick=\"empty('w3w:$row[recordID]'); getAPRSLocations('$row[aprs_call], $row[recordID], $row[latitude],$row[longitude],$row[callsign],$row[netID], W3W');\">
-            <div class='$class'> $row[w3w]	</div> 
+            onClick=\"empty('w3w:$row[recordID]'); getAPRSLocations('$row[aprs_call], $row[recordID], $row[latitude],$row[longitude],$row[callsign],$row[netID], W3W');\" 
+            style=\"cursor: pointer;\">
+            <div class='$class' readonly> $row[w3w]	</div> 
         </td> 
     
         <td $brbCo
